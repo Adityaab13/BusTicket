f6ce32c44c097664e9aacd1fa095b4a275b56378
Adityaab13 <adityabadiger1997@gmail.com>
2021-12-06 15:31:23 -0500

    final star js

--- components/StarRating/Rating.js
+++ components/StarRating/Rating.js
@@ -68,3 +68,16 @@ const Rating = ({ value, text, color }) => {
     </div>
     )
 }
+
+Rating.defaultProps = {
+    color: '#f8e825',
+  }
+  
+  Rating.propTypes = {
+    value: PropTypes.number.isRequired,
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string,
+  }
+  
+  export default Rating;
+  
